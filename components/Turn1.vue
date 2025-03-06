2e8129caa117fb44a0deb05ddb69c8be530247c4
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2025-03-05 23:04:34 -0300

    Atualizado página de visão geral

--- components/Turn1.vue
+++ components/Turn1.vue
@@ -13,41 +13,7 @@
         <div
           class="w-5 h-5 text-xs text-center text-white flex items-center justify-center rounded-full bg-red-600"
         >
-          6
-        </div>
-        <div
-          class="w-5 h-5 text-xs text-center text-white flex items-center justify-center rounded-full bg-red-600"
-        >
           12
-        </div>
-        <div
-          class="w-5 h-5 text-xs text-center text-white flex items-center justify-center rounded-full bg-green-600"
-        >
-          18
-        </div>
-      </div>
-      <div
-        class="flex gap-x-0.5 p-1.5 rounded-full hover:bg-neutral-100 dark:hover:bg-gray-700"
-      >
-        <div
-          class="w-5 h-5 text-xs text-center text-white flex items-center justify-center rounded-full bg-green-600"
-        >
-          0
-        </div>
-        <div
-          class="w-5 h-5 text-xs text-center text-white flex items-center justify-center rounded-full bg-orange-500"
-        >
-          6
-        </div>
-        <div
-          class="w-5 h-5 text-xs text-center text-white flex items-center justify-center rounded-full bg-green-600"
-        >
-          12
-        </div>
-        <div
-          class="w-5 h-5 text-xs text-center text-white flex items-center justify-center rounded-full bg-orange-500"
-        >
-          18
         </div>
       </div>
       <div
@@ -61,17 +27,21 @@
         <div
           class="w-5 h-5 text-xs text-center text-white flex items-center justify-center rounded-full bg-green-600"
         >
-          6
+          12
+        </div>
+      </div>
+      <div
+        class="flex gap-x-0.5 p-1.5 rounded-full hover:bg-neutral-100 dark:hover:bg-gray-700"
+      >
+        <div
+          class="w-5 h-5 text-xs text-center text-white flex items-center justify-center rounded-full bg-green-600"
+        >
+          0
         </div>
         <div
           class="w-5 h-5 text-xs text-center text-neutral-500 flex items-center justify-center rounded-full bg-neutral-100 dark:text-neutral-200 dark:bg-neutral-700"
         >
           12
-        </div>
-        <div
-          class="w-5 h-5 text-xs text-center text-neutral-500 flex items-center justify-center rounded-full bg-neutral-100 dark:text-neutral-200 dark:bg-neutral-700"
-        >
-          18
         </div>
       </div>
     </div>
