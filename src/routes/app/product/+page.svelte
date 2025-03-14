ec5dde8280350677f6d26f92430cabf0e0a615b0
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2025-03-14 01:38:10 -0300

    Criado a página de produto

---
+++ src/routes/app/product/+page.svelte
@@ -0,0 +1,14 @@
+<div class="flex min-h-[calc(100vh-64px)] w-full bg-white dark:bg-neutral-900">
+	<!-- Side left -->
+	<div class="flex w-[320px] flex-shrink-0 flex-col border-r border-neutral-200 dark:border-neutral-700">
+		<div class="p-8">
+			<h1>Árvore de dependências</h1>
+		</div>
+	</div>
+	<!-- Side right -->
+	<div class="flex flex-grow flex-col">
+		<div class="p-8">
+			<h1>Detalhes do produto</h1>
+		</div>
+	</div>
+</div>
