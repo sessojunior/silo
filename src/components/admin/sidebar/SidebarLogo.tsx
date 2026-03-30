cd9e894e3310f3708518c26093d055d29dc105b3
Mario Junior <sessojunior@gmail.com>
2026-03-30 11:15:53 -0300

    Corrigido versão

--- src/components/admin/sidebar/SidebarLogo.tsx
+++ src/components/admin/sidebar/SidebarLogo.tsx
@@ -4,7 +4,7 @@ import { config as appConfig } from "@/lib/config";
 export default function SidebarLogo() {
   const logoSrc = appConfig.getPublicPath("/images/logo.png");
 
-  const VERSION = "26.3.27.1"; // Formato da versão é "Ano.Mês.Dia.Contagem"
+  const VERSION = "26.3.30.1"; // Formato da versão é "Ano.Mês.Dia.Contagem"
 
   return (
     <div className="flex items-center">
